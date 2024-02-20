cfe9d2f5f2ef3e392e0bb5c3f902def436ec14c3
Spencer Smolen <spencer.smolen@scientificgames.com>
2024-02-19 23:24:20 -0500

    added diffview

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  vim.keymap.set("n", "<leader><leader>v", function()
+    if next(require("diffview.lib").views) == nil then
+      vim.cmd("DiffviewOpen")
+    else
+      vim.cmd("DiffviewClose")
+    end
+  end),
+}
