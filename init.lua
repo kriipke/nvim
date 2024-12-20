d7509711bbf88e84b887e36efc881790bbbbbb18
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2024-12-20 11:14:21 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,8 +2,5 @@ require("config.lazy")
 
 vim.opt.termguicolors = true
 vim.o.background = "dark"
-vim.cmd.colorscheme("PaperColor")
--- vim.cmd.colorscheme("oxocarbon")
--- vim.cmd.colorscheme("monochrome")
--- vim.cmd.colorscheme("nightcity-kabuki")
+vim.cmd.colorscheme("ayu-dark")
 require("bufferline").setup({})
